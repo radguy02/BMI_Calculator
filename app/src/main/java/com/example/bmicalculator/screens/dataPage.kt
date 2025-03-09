b9dff275d37ba1a18ad8c1eddb2140bd3f48b67b
RADGuy <anishraj365@gmail.com>
2025-03-09 12:19:59 +0530

    Changes to datapage

--- app/src/main/java/com/example/bmicalculator/screens/dataPage.kt
+++ app/src/main/java/com/example/bmicalculator/screens/dataPage.kt
@@ -14,8 +14,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,7 +31,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,99 +40,145 @@ import com.example.bmicalculator.R
 @Composable
 fun Datapage() {
     var showInfo by remember { mutableStateOf(false) }
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(16.dp)
-    ) {
-        Column(
+    Scaffold { paddingValues ->
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
+                .padding(paddingValues)
         ) {
-            Text(
-                "Enter your details",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onBackground,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(500.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(horizontal = 8.dp)
             ) {
-                Box(
+                Row(
                     modifier = Modifier
-                        .fillMaxHeight()
                         .weight(1f)
-                        .background(
-                            MaterialTheme.colorScheme.surface,
-                            shape = RoundedCornerShape(12.dp)
-                        )
-                        .border(2.dp, Color(0xFF002D67), RoundedCornerShape(12.dp))
+                        .fillMaxWidth(.7f),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.Center
                 ) {
+                    Text(
+                        "Enter your details",
+                        fontFamily = FontFamily(Font(R.font.lilitaoneregular)),
+                        fontSize = 30.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(7.5f),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(.40f)
+                            .background(
+                                MaterialTheme.colorScheme.surface, shape = RoundedCornerShape(12.dp)
+                            )
+                            .border(2.dp, Color(0xFF002D67), RoundedCornerShape(12.dp))
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        ) {
+                            Text(
+                                "Height",
+                                fontFamily = FontFamily(Font(R.font.lilitaoneregular)),
+                                fontSize = 24.sp,
+                                color = MaterialTheme.colorScheme.onBackground,
+                                modifier = Modifier.align(Alignment.CenterHorizontally)
+                            )
+                        }
+                        Column { /* slider code */ }
+                    }
                     Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
+                            .fillMaxHeight()
+                            .weight(.60f)
                     ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                                .background(
+                                    MaterialTheme.colorScheme.surface,
+                                    shape = RoundedCornerShape(12.dp)
+                                )
+                                .border(2.dp, Color(0xFF002D67), RoundedCornerShape(12.dp))
+                        ) {
+                            Column(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)) {
+                                Text(
+                                    "Age",
+                                    fontFamily = FontFamily(Font(R.font.lilitaoneregular)),
+                                    fontSize = 24.sp,
+                                    color = MaterialTheme.colorScheme.onBackground,
+                                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                                )
+                            }
+                        }
+                        Spacer(Modifier.size(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                                .background(
+                                    MaterialTheme.colorScheme.surface,
+                                    shape = RoundedCornerShape(12.dp)
+                                )
+                                .border(2.dp, Color(0xFF002D67), RoundedCornerShape(12.dp))
+                        ) {
+                            Column(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)) {
+                                Text(
+                                    "Weight",
+                                    fontFamily = FontFamily(Font(R.font.lilitaoneregular)),
+                                    fontSize = 24.sp,
+                                    color = MaterialTheme.colorScheme.onBackground,
+                                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                                )
+                            }
+                        }
+                    }
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1.5f),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ElevatedButton(onClick = { "" }) {
                         Text(
-                            "Height",
+                            "Calculate",
                             fontFamily = FontFamily(Font(R.font.lilitaoneregular)),
                             fontSize = 24.sp,
-                            color = MaterialTheme.colorScheme.onBackground,
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                            color = MaterialTheme.colorScheme.onBackground
                         )
-                    }
-                    Column { /* slider code */ }
-                }
-                Column(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .weight(1f)
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                            .background(
-                                MaterialTheme.colorScheme.surface,
-                                shape = RoundedCornerShape(12.dp)
-                            )
-                            .border(2.dp, Color(0xFF002D67), RoundedCornerShape(12.dp))
-                    ) {
-                    }
-                    Spacer(Modifier.size(8.dp))
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                            .background(
-                                MaterialTheme.colorScheme.surface,
-                                shape = RoundedCornerShape(12.dp)
-                            )
-                            .border(2.dp, Color(0xFF002D67), RoundedCornerShape(12.dp))
-                    ) {
                     }
                 }
             }
-            Box() { Text("RADGuy") }
-        }
 
 
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ) {
-            if (showInfo) {
-                Text(
-                    text = """
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                if (showInfo) {
+                    Text(
+                        text = """
                         Underweight: BMI < 18.5
                         Healthy weight: BMI 18.5 - 24.9
                         Overweight: BMI 25 - 29.9
@@ -139,37 +186,37 @@ fun Datapage() {
                           • Class 1: BMI 30 - <35
                           • Class 2: BMI 35 - <40
                           • Class 3: BMI ≥ 40""".trimIndent(),
-                    color = MaterialTheme.colorScheme.onSurface,
-                    fontSize = 14.sp,
-                    modifier = Modifier
-                        .background(
-                            MaterialTheme.colorScheme.surface.copy(alpha = 0.8f),
-                            shape = RoundedCornerShape(8.dp)
-                        )
-                        .padding(8.dp)
-                )
+                        color = MaterialTheme.colorScheme.onSurface,
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .background(
+                                MaterialTheme.colorScheme.surface.copy(alpha = 0.8f),
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .padding(8.dp)
+                    )
 
-                Spacer(modifier = Modifier.height(24.dp))
+                    Spacer(modifier = Modifier.height(24.dp))
+                }
             }
-        }
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .background(Color.Transparent)
-                .size(32.dp)
-        ) {
-            SmallFloatingActionButton(
-                onClick = { showInfo = !showInfo },
-                containerColor = MaterialTheme.colorScheme.surface
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .background(Color.Transparent)
+                    .size(32.dp)
             ) {
-                Icon(
-                    painter = painterResource(R.drawable.help),
-                    contentDescription = "Help",
-                    tint = MaterialTheme.colorScheme.onPrimary
+                SmallFloatingActionButton(
+                    onClick = { showInfo = !showInfo },
+                    containerColor = MaterialTheme.colorScheme.surface
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.help),
+                        contentDescription = "Help",
+                        tint = MaterialTheme.colorScheme.onPrimary
 
-                )
+                    )
+                }
             }
         }
     }
 }
-
---
+++ app/src/main/java/com/example/bmicalculator/screens/sliderrr.kt
@@ -0,0 +1,113 @@
+package com.example.bmicalculator.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderColors
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun VerticalSliderExample() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column {
+            var value by remember { mutableStateOf(0f) }
+            VerticalSlider(
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp)
+                    .background(Color(0xffdedede))
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            Slider(
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp)
+                    .background(Color(0xffdedede))
+            )
+        }
+    }
+}
+
+
+@Composable
+fun VerticalSlider(
+    value: Float,
+    onValueChange: (Float) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
+    /*@IntRange(from = 0)*/
+    steps: Int = 0,
+    onValueChangeFinished: (() -> Unit)? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    colors: SliderColors = SliderDefaults.colors()
+){
+    Slider(
+        colors = colors,
+        interactionSource = interactionSource,
+        onValueChangeFinished = onValueChangeFinished,
+        steps = steps,
+        valueRange = valueRange,
+        enabled = enabled,
+        value = value,
+        onValueChange = onValueChange,
+        modifier = Modifier
+            .graphicsLayer {
+                rotationZ = 270f
+                transformOrigin = TransformOrigin(0f, 0f)
+            }
+            .layout { measurable, constraints ->
+                val placeable = measurable.measure(
+                    Constraints(
+                        minWidth = constraints.minHeight,
+                        maxWidth = constraints.maxHeight,
+                        minHeight = constraints.minWidth,
+                        maxHeight = constraints.maxHeight,
+                    )
+                )
+                layout(placeable.height, placeable.width) {
+                    placeable.place(-placeable.width, 0)
+                }
+            }
+            .then(modifier)
+    )
+}
